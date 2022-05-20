8c050f56ae99ff764e36fec1cf7b8f49e91ff36e
Alessandro Zanola <ales.zanola@gmail.com>
2022-05-20 15:18:44 +0200

    Init UI in registration page with bootstrap

--- components.d.ts
+++ components.d.ts
@@ -5,17 +5,21 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    AppLayout: typeof import('./src/components/AppLayout.vue')['default']
+    HelloWorld: typeof import('./src/components/old_components/HelloWorld.vue')['default']
+    IconCommunity: typeof import('./src/components/old_components/icons/IconCommunity.vue')['default']
+    IconDocumentation: typeof import('./src/components/old_components/icons/IconDocumentation.vue')['default']
+    IconEcosystem: typeof import('./src/components/old_components/icons/IconEcosystem.vue')['default']
+    IconSupport: typeof import('./src/components/old_components/icons/IconSupport.vue')['default']
+    IconTooling: typeof import('./src/components/old_components/icons/IconTooling.vue')['default']
+    NavBar: typeof import('./src/components/NavBar.vue')['default']
+    NButton: typeof import('naive-ui')['NButton']
     NInput: typeof import('naive-ui')['NInput']
+    NSpace: typeof import('naive-ui')['NSpace']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
+    TheWelcome: typeof import('./src/components/old_components/TheWelcome.vue')['default']
+    WelcomeItem: typeof import('./src/components/old_components/WelcomeItem.vue')['default']
   }
 }
 
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,22 +1,61 @@
 <template>
-  <div>
-    <n-input v-model:value="text"></n-input>
-    {{ text }}
+  <div class="m-3">
+    <h2>Sign in</h2>
+
+    <b-form>
+      <div class="mb-2">
+        <span>Nome</span>
+        <b-form-input v-model="user.name" />
+      </div>
+
+      <div class="mb-2">
+        <span>Surname</span>
+        <b-form-input v-model="user.surname" />
+      </div>
+
+      <div class="mb-2">
+        <span>Email</span>
+        <b-form-input v-model="user.email" />
+      </div>
+
+      <div class="mb-2">
+        <span>Password</span>
+        <b-form-input v-model="user.password" />
+      </div>
+
+      <b-button class="float-end" @click.prevent="register">Sign in</b-button>
+    </b-form>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import {BButton, BForm, BFormInput} from "bootstrap-vue-3";
 
 export default defineComponent({
   name: "RegisterView",
+  components: {BButton, BFormInput, BForm},
   setup() {
     return {
-      text: ref("Questo te lo scrivo da codice"),
+      user: ref({
+        name: '',
+        surname: '',
+        email: '',
+        password: '',
+      }),
       data: [1, 5, 7],
     };
   },
+  methods: {
+    register() {
+      console.log('Sign in')
+    }
+  }
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.my-btn {
+  width: 400px;
+}
+</style>
